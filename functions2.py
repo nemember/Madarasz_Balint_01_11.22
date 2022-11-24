519e292f83ab3f0a56dd19c113d3aebb469f9deb
nemember <madarasz.balint@students.jedlik.eu>
2022-11-24 12:43:26 +0100

    5

--- functions2.py
+++ functions2.py
@@ -1,4 +1,5 @@
 from os import system
+from meh import termekek
 
 filename='Termékek.csv'
 
@@ -11,3 +12,36 @@ def menu():
     print('3 - Kosár')
     print('4 - Kosárból törlés')
     return input('Válsztás: ')
+
+file=""
+
+
+def Betoltes():
+    file=open(filename, 'r', encoding='utf-8')
+    for row in file:
+        splitted=row.strip().split(';')
+        termekek[splitted[0]]=int(splitted[1])
+    file.close()
+
+
+def Termekekkiirasa():
+    system('cls')
+    print('A termékék listája:\n ')
+    sorsszam=1
+    for key, value in termekek.items():
+       print(f'{sorsszam} - {key} - {value}Ft')
+       sorsszam+=1
+    input('Tovább (Enter)')
+
+def Kosarbarakas():
+    system('cls')
+    print('A termékék listája:\n ')
+    for key, value in termekek.items():
+       print(f'{key} - {value}Ft')
+    valasztas=input('Melyik terméket szeretné a kosárba helyezni (A sorszámát írja be)')
+    
+    
+
+  
+
+ 
--- main2.py
+++ main2.py
@@ -1,13 +1,19 @@
 from functions2 import *
 
 
+Betoltes()
+
 
 choice=''
-while choice !=0:
+while choice !="0":
     choice=menu()
     if choice=='1':
-        pass
+        Termekekkiirasa()
     elif choice=='2':
         pass
     elif choice=='3':
+        pass
+    elif choice=='4':
+        pass
+    elif choice=='4':
         pass
